eeeb21d55ef867f3a5593a5b058aad9015bdbfde
sumirart <akun.pratamasumirat@gmail.com>
2018-12-20 08:02:53 +0700

    fix search page

--- src/movie/screens/search.js
+++ src/movie/screens/search.js
@@ -54,53 +54,63 @@ class Search extends Component {
         return (
             <section id="semua" style={{ marginTop: 90, marginBottom: "5vh" }}>
                 <Container>
-                    <div className="row p-3">
-                        <div className="col-md-12">
-                            <h2 className="text-left">Search for: {values.search}</h2>
+                    {values.search === '' ?
+                        <div className="row p-3" style={{ padding: 0 }}>
+                            <div className="col-md-12" style={{ padding: 0 }}>
+                                <h2 className="text-center"
+                                    style={{ fontSize: 30, fontWeight: 600, lineHeight: 1.4 }}>Please enter keyword!</h2>
+                            </div>
                         </div>
-                    </div>
-                    {
-                        this.state.lastPage === 0 ?
-                            <div className="row p-3">
-                                <div className="col-md-12">
-                                    <h2 className="text-left">No movies found!</h2>
+                        : (
+                            <div>
+                                <div className="row p-3" style={{ padding: 0 }}>
+                                    <div className="col-md-12" style={{ padding: 0 }}>
+                                        <h2 className="text-left"
+                                            style={{ fontSize: 30, fontWeight: 600, lineHeight: 1.4 }}>Search for: {values.search} </h2>
+                                    </div>
+                                </div>
+                                {
+                                    this.state.lastPage === 0 ?
+                                        <div className="row p-3">
+                                            <div className="col-md-12">
+                                                <h2 className="text-left">No movies found!</h2>
+                                            </div>
+                                        </div>
+                                        :
+                                        <CardGroup>
+                                            <div className="row" style={{ marginBottom: 10 }}>
+                                                {
+                                                    this.state.movies.map(data =>
+                                                        <div key={data.id} className="Item" style={{ backgroundImage: 'url(' + data.thumbnails + ')' }} >
+                                                            <Link to={{ pathname: '/movie/' + data.slug, state: data }} data={data} style={{ color: "white", textDecoration: "none" }}>
+                                                                <div className="overlay">
+                                                                    <div className="title" style={{ lineHeight: 1.2 }}>{data.title.replace(/(^\Nonton +|\ Subtitle Indonesia+$)/mg, '')}</div>
+                                                                    <div className="rating">
+                                                                        {data.rating ?
+                                                                            data.rating.substr(0, 3) + ' / 10'
+                                                                            : 'no rating'
+                                                                        }
+                                                                    </div>
+                                                                    <div className="plot">{data.description.substr(0, 100) + '..'}</div>
+                                                                </div>
+                                                            </Link>
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                        </CardGroup>
+                                }
+                                <div className="col-md-12" align="center" style={{ marginBottom: 20 }}>
+                                    {this.state.page === this.state.lastPage || this.state.lastPage === 0 || this.state.lastPage === 1 ?
+                                        <Button color="secondary" size="large" target="_blank" >Load More</Button>
+                                        :
+                                        <Button onClick={this.fetchMore} color="success" size="large" target="_blank">
+                                            Load More</Button>
+                                    }
                                 </div>
                             </div>
-                            :
-                            <CardGroup>
-                                <div className="row" style={{ marginBottom: 10 }}>
-                                    {
-                                        this.state.movies.map(data =>
-                                            <Item key={data.id}
-                                                title={data.title.substr(0, data.title.indexOf('('))}
-                                                score={data.rating.substr(0, 3)}
-                                                overview={data.description}
-                                                backdrop={data.thumbnails}
-                                            />
-                                        )
-                                    }
-                                    {/* previous clickable item
-                                    <div className="col-sm-6 col-md-4 col-lg-2" key={data.id}>
-                                        <Card style={{ marginBottom: 20 }}>
-                                            <CardImg top width="100%" src={data.thumbnails} alt={data.title} />
-                                            <CardBody>
-                                                <CardTitle>{data.title}</CardTitle>
-                                                <CardText style={{ maxHeight: 200, overflow: "hidden" }}>{data.description}</CardText>
-                                                <Link to={{ pathname: '/movie/' + data.slug, state: data }} className="btn btn-primary btn-sm float-right" data={data} >Tonton</Link>
-                                            </CardBody>
-                                        </Card>
-                                    </div> */}
-                                </div>
-                            </CardGroup>
+                        )
                     }
-                    <div className="col-md-12" align="center" style={{ marginBottom: 20 }}>
-                        {this.state.page === this.state.lastPage || this.state.lastPage === 0 || this.state.lastPage === 1 ?
-                            <Button color="secondary" size="large" target="_blank" >Load More</Button>
-                            :
-                            <Button onClick={this.fetchMore} color="success" size="large" target="_blank">
-                                Load More</Button>
-                        }
-                    </div>
                 </Container>
             </section>
         );
