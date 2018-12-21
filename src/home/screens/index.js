6578a6dd2fe1e97e8925656df624ac2f789a9a9a
sumirart <akun.pratamasumirat@gmail.com>
2018-12-21 19:51:47 +0700

    add onesignal

--- src/home/screens/index.js
+++ src/home/screens/index.js
@@ -207,11 +207,11 @@ class Home extends Component {
               />
             </div>
           </div>
-          <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <a className="carousel-control-prev" role="button" data-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="sr-only">Previous</span>
           </a>
-          <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <a className="carousel-control-next" role="button" data-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="sr-only">Next</span>
           </a>
